a5b91090eb7c12231639506337449489c77b6143
taylorJ <taylor.ji719@gmail.com>
2022-12-29 14:56:29 -0800

    Initial Commit

---
+++ Quiz/QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizApp.swift
+//  Quiz
+//
+//  Created by Taylor Ji on 2022-12-29.
+//
+
+import SwiftUI
+
+@main
+struct QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
